76d6c3c006ee8f4cbf4931e08a83b4db508b1757
Santhosh D <santhosh.d@disprz.com>
2025-10-25 18:53:50 +0530

    Fix:POST,PUT_Config_&POST,PUT_Layout

--- FormBuilderAPITests/obj/Debug/net9.0/FormBuilderAPITests.AssemblyInfo.cs
+++ FormBuilderAPITests/obj/Debug/net9.0/FormBuilderAPITests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FormBuilderAPITests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0036c13395c91ecffcae498e56cf09bf2e46f2e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef6de18be151d8d3ef9588f1dd20c00cbd73ea0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FormBuilderAPITests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FormBuilderAPITests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
